98549161150f41b38fa55e27fe950831c1867697
Damir <bajrami.damir@gmail.com>
2024-12-18 16:25:09 +0100

    added a Link component

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,34 @@
+import { FC, ReactNode } from "react";
+import "./Link.css";
+
+interface LinkProps {
+  href: string;
+  children: ReactNode;
+  target?: "_blank" | "_self" | "_parent" | "_top";
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const Link: FC<LinkProps> = ({ href, children, target, disabled, onClick }) => {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (disabled) {
+      e.preventDefault(); // Prevent navigation if disabled
+      return;
+    }
+    onClick?.();
+  };
+
+  return (
+    <a
+      href={disabled ? undefined : href}
+      target={target}
+      onClick={handleClick}
+      className={`link ${disabled ? "link-disabled" : ""}`}
+      aria-disabled={disabled}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default Link;
---
+++ src/stories/Link.stories.tsx
@@ -0,0 +1,26 @@
+import Link from "../components/Link/Link";
+import Text from "../components/Text/Text";
+
+export default {
+  title: "Components/Link",
+  component: Link,
+};
+
+export const Default = () => (
+  <Link href="https://example.com" target="_blank">
+    Visit Example
+  </Link>
+);
+
+export const InheritStylesFromText = () => (
+  <Text variant="h1">
+    This is a paragraph with a <Link href="https://example.com">link</Link>{" "}
+    inside it (it inherits the styles of the parent Text component :) )
+  </Text>
+);
+
+export const Disabled = () => (
+  <Link href="https://example.com" disabled>
+    Disabled Link
+  </Link>
+);
