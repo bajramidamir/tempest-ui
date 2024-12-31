3d8232c885928cfea63d6a5da6975cb8360ecae6
Damir <bajrami.damir@gmail.com>
2024-12-31 11:59:40 +0100

    smallest fix ever (0.9rem -> 1rem)

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Table from "../components/Table/Table";
-import Button from "../components/Button/Button";
 
 export default {
   title: "Components/Table",
@@ -8,7 +7,7 @@ export default {
 };
 
 export const Default = () => {
-  const columns = ["name", "age", "email", ""];
+  const columns = ["name", "age", "email"];
   const data = [
     { name: "Alice", age: 25, email: "alice@example.com" },
     { name: "Bob", age: 30, email: "bob@example.com" },
